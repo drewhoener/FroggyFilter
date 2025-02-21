c01999b26dda487354f948c303bd137ea50ab8cf
Drew Hoener <drewhoener@gmail.com>
2025-02-21 16:49:46 -0500

    Add option to respect monster eradication goals

--- Framework/ModConfig.cs
+++ Framework/ModConfig.cs
@@ -4,6 +4,7 @@ namespace FroggyFilter.Framework;
 
 public class ModConfig
 {
+    public bool ExcludeEradicationGoals { get; set; } = false;
     public Dictionary<string, bool> EnabledMonsters { get; set; } = new();
     public string ExcludedMonsters { get; set; } = string.Empty;
 }
